69c2ef18be1df9a3b00ec3a12eba8e2aad461ab8
salahmalih <C130084552@taalim.ma>
2023-12-03 10:57:31 +0100

    test

--- function3.c
+++ function3.c
@@ -24,16 +24,15 @@ int print_unsigned(va_list l, flags_t *f)
  */
 int print_octal(va_list l, flags_t *f)
 {
-    unsigned int octal;
-    int count = 0;
+	unsigned int num = va_arg(l, unsigned int);
+	char *str;
+	int count = 0;
 
-    octal = va_arg(l, unsigned int);
-    if (f->hash && octal != 0)
-        count += _putchar('0');
-
-    count += print_number(octal);
-
-    return (count);
+    str = convert(num, 8, 0);
+	if (f->hash == 1 && str[0] != '0')
+		count += _putchar('0');
+	count += _puts(str);
+	return (count);
 }
 
 /**
