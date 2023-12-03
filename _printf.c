c939656f85647c0a3410545149201bb766badd47
salahmalih <C130084552@taalim.ma>
2023-12-03 02:21:20 +0100

    please good

--- _printf.c
+++ _printf.c
@@ -7,38 +7,37 @@
  */
 int _printf(const char *format, ...)
 {
-	int (*pfunc)(va_list, flags_t *);
-	const char *p;
-	va_list arguments;
-	flags_t flags = {0, 0, 0, 0, 0, 0, 0};
+    va_list args;
+    int count = 0;
+     flags_t flags = {0, 0, 0, 0, 0, 0, 0};
+    int (*printer)(va_list, flags_t *);
 
-	register int count = 0;
-
-	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
+    va_start(args, format);
+    printer= get_print(*format);
+    if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-	for (p = format; *p; p++)
-	{
-		if (*p == '%')
-		{
-			p++;
-			if (*p == '%')
-			{
-				count += _putchar('%');
-				continue;
-			}
-			while (get_flag(*p, &flags))
-				p++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
-			count += _putchar(*p);
-	}
-	_putchar(-1);
-	va_end(arguments);
-	return (count);
+    while (format && *format)
+    {
+        if (*format == '%')
+            {
+                format++;
+                if (*format == '%')
+                {
+                    count += _putchar('%');
+                    continue;
+                }
+                while (get_flag(*format, &flags))
+                    format++;
+                printer = get_print(*format);
+                count += (printer)
+                    ? printer(args, &flags)
+                    : _printf("%%%c", *format);
+            } else
+                count += _putchar(*format);
+    }
+    _putchar(-1);
+    va_end(args);
+    return (count);
 }
