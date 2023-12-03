a327a4bddd5de08ab27fac00f63ee443641426fb
salahmalih <C130084552@taalim.ma>
2023-12-03 02:11:00 +0100

    please good

--- get_flag.c
+++ get_flag.c
@@ -8,26 +8,33 @@
  */
 int get_flag(char s, flags_t *f)
 {
+    int i = 0;
+
     switch (s)
     {
     case '+':
         f->plus = 1;
+        i = 1;
         break;
     case ' ':
         f->space = 1;
+        i = 1;
         break;
     case '#':
         f->hash = 1;
+        i = 1;
         break;
     case '0':
         f->zero = 1;
+        i = 1;
         break;
     case '-':
         f->minus = 1;
+        i = 1;
         break;
     default:
         return (-1);
     }
 
-    return (0);
+    return (i);
 }
