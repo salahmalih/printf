d058a8b8a84572ccd742331b416f81a842250f8a
salahmalih <C130084552@taalim.ma>
2023-12-03 11:37:32 -0600

    fix task 0

--- main.h
+++ main.h
@@ -46,7 +46,6 @@ typedef struct format fmt_t;
 int print_int(va_list l, flags_t *f);
 int print_number(int n);
 int print_unsigned(va_list l, flags_t *f);
-int count_digit(int i);
 
 /* print_bases */
 int print_hex(va_list l, flags_t *f);
