3b9f1324904d1e338e66a959aeef8ab59206358d
salahmalih <C130084552@taalim.ma>
2023-12-03 02:29:07 +0100

    please good

--- _printf.c
+++ _printf.c
@@ -1,38 +1,56 @@
 #include "main.h"
+
 /**
  * _printf - Produces output according to a format.
- * @format: The format string
- * @...: Variable arguments
- * Return: The number of characters printed (excluding the null byte)
+ * @format: Format string containing the characters and specifiers.
+ * Return: Length of the formatted output string.
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
-    flags_t flags = {0, 0, 0, 0, 0, 0, 0};
     int (*printer)(va_list, flags_t *);
+    const char *p;
+    va_list arguments;
+    flags_t flags = {0, 0, 0, 0, 0, 0, 0};  // Initialize all flags to 0.
 
-    va_start(args, format);
-    while (format && *format)
+    register int count = 0;
+
+    va_start(arguments, format);
+
+    if (!format || (format[0] == '%' && !format[1]))
+        return (-1);
+
+    for (p = format; *p; p++)
     {
-        if (*format == '%')
+        if (*p == '%')
+        {
+            p++;
+            if (*p == '%')
             {
-                format++;
-                if (*format == '%')
-                {
-                    count += _putchar('%');
-                    continue;
-                }
-                while (get_flag(*format, &flags))
-                    format++;
-                printer = get_print(*format);
-                count += (printer)
-                    ? printer(args, &flags)
-                    : _printf("%%%c", *format);
-            } else
-                count += _putchar(*format);
+                count += _putchar('%');
+                continue;
+            }
+
+            // Reset flags for each new specifier.
+            flags = (flags_t){0, 0, 0, 0, 0, 0, 0};
+
+            // Process flags.
+            while (get_flag(*p, &flags))
+                p++;
+
+            printer = get_print(*p);
+
+            if (printer != NULL)
+                count += printer(arguments, &flags);
+            else
+                count += _printf("%%%c", *p);
+        }
+        else
+        {
+            count += _putchar(*p);
+        }
     }
-    _putchar(-1);
-    va_end(args);
+
+    va_end(arguments);
+
     return (count);
 }
